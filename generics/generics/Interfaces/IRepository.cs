e192a904a436c164cd1b2280b9a83e3de9e1c6dc
melissa-900 <rud.melissa@lll.kpi.ua>
2025-05-13 17:52:07 +0300

    5

--- generics/generics/Interfaces/IRepository.cs
+++ generics/generics/Interfaces/IRepository.cs
@@ -1,19 +1,33 @@
 ﻿namespace generics.Interfaces
 {
-    interface IRepository<TEntity, TKey> where TEntity : class, new() where TKey    : struct
-    { 
+    interface IRepository<TEntity, TKey> where TEntity : class, new() where TKey : struct
+    {
         void Add(TKey id, TEntity entity);
         TEntity Get(TKey id);
         IEnumerable<TEntity> GetAll();
-        void Remove(TKey id); 
+        void Remove(TKey id);
     }
 
-    class InMemoryRepository<TEntity,TKey> : IRepository<TEntity,TKey>, IReadOnlyRepository<TEntity,TKey> where TEntity : class, new() where TKey    : struct {
+    class InMemoryRepository<TEntity, TKey> : IRepository<TEntity, TKey>, IReadOnlyRepository<TEntity, TKey>, IWriteRepository<TEntity, TKey> where TEntity : class, new() where TKey : struct
+    {
         private Dictionary<TKey, TEntity> _entities = new Dictionary<TKey, TEntity>();
+        static int counter;
+        static InMemoryRepository()
+        {
+            counter = 0;
+        }
+
+        public void Add(TEntity entity)
+        {
+            TKey id = (TKey)Convert.ChangeType(counter, typeof(TKey));
+            _entities.Add(id, entity);
+            counter++;
+        }
 
         public void Add(TKey id, TEntity entity)
         {
             _entities.Add(id, entity);
+            counter++;
         }
 
         public TEntity Get(TKey id)
@@ -29,11 +43,19 @@
         public void Remove(TKey id)
         {
             _entities.Remove(id);
+            counter--;
         }
     }
 
-    interface IReadOnlyRepository<out TEntity, in TKey> {
+    interface IReadOnlyRepository<out TEntity, in TKey>
+    {
         TEntity Get(TKey id);
         IEnumerable<TEntity> GetAll();
+    }
+
+    interface IWriteRepository<in TEntity, in TKey>
+    {
+        void Add(TEntity entity);
+        void Remove(TKey id);
     }
 }
--- generics/generics/Program.cs
+++ generics/generics/Program.cs
@@ -17,16 +17,21 @@ class Program
         Student Melissa = new Student { Id = 1, Name = "Melisa"};
         Student Student1 = new Student { Id = 2, Name = "1"};
         Student Student2 = new Student { Id = 3, Name = "2"};
+        Student Student3 = new Student { Id = 3, Name = "3"};
 
         KP41.AddStudent(Melissa);
         KP41.AddStudent(Student1);
         KP41.AddStudent(Student2);
+        KP42.AddStudent(Student3);
         
-        IEnumerable<Student> studentsKP41 = KP41.GetAllStudents();
-        Console.WriteLine(studentsKP41);
+        KP41.GetAllStudents();
 
         // 4
         IReadOnlyRepository<Student,int> studRepo = new InMemoryRepository<Student,int>();
         IReadOnlyRepository<Person,int>  persRepo = studRepo;  // має компілюватися
+
+        // 5
+        IWriteRepository<Student,int> persWrite = new InMemoryRepository<Student,int>();
+        IWriteRepository<Student,int> studWrite = persWrite;  // має компілюватися
     }
 }
